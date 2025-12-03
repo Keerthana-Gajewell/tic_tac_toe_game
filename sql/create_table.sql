24d6ae4c35b32136a4b40796a38933e904804844
Keerthana Gajewell <Gajwelkeerthana@gmail.com>
2025-12-03 23:39:59 +0530

    Delete sql directory

--- sql/create_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS webscraper_db;
-USE webscraper_db;
-
-CREATE TABLE IF NOT EXISTS quotes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    quote TEXT,
-    author VARCHAR(255),
-    tags TEXT
-);
